16714fefceabcc275a6d031926144c4d80ef2f24
Corey Sheesley <corey.sheesley@gmail.com>
2019-02-07 11:37:38 -0700

    Refactors using attr_readers, removes individual methods in class

--- lib/apartment.rb
+++ lib/apartment.rb
@@ -1,25 +1,12 @@
 class Apartment
-  attr_reader :renter
+  attr_reader :number, :monthly_rent, :bathrooms, :bedrooms, :renter
 
   def initialize(apartment_info)
-    @apartment_info = apartment_info
+    @number = apartment_info[:number]
+    @monthly_rent = apartment_info[:monthly_rent]
+    @bathrooms = apartment_info[:bathrooms]
+    @bedrooms = apartment_info[:bedrooms]
     @renter = nil
-  end
-
-  def number
-    @apartment_info[:number]
-  end
-
-  def monthly_rent
-    @apartment_info[:monthly_rent]
-  end
-
-  def bathrooms
-    @apartment_info[:bathrooms]
-  end
-
-  def bedrooms
-    @apartment_info[:bedrooms]
   end
 
   def add_renter(renter)
